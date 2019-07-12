f5dbd9270781f98b7a6e5d4133feff9ec1a95eb7
Felix Breuer <f.breuer94@gmail.com>
2019-07-12 17:03:48 +0200

    init svelte and ranking

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 15%;
+		box-sizing: border-box;
+	}
+</style>
+
+<svelte:head>
+	<title>elo hell</title>
+</svelte:head>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,37 @@
+<script context='module'>
+	export async function preload(page, session) {
+		const res = await this.fetch('user.json');
+		const users = await res.json();
+		return { users };
+	}
+</script>
+
+<script>
+	export let users;
+</script>
+
+<style>
+	table {
+		width: 100%;
+	}
+
+	td, th {
+		text-align: center;
+		vertical-align: middle;
+	}
+</style>
+
+<table>
+	<tr>
+		<th># rank</th>
+		<th># username</th>
+		<th># elo</th> 
+	</tr>
+	{#each users as user, rank}
+		<tr>
+			<td>{rank}</td>
+			<td>{user.username}</td>
+			<td>{user.elo}</td> 
+		</tr>
+	{/each}
+</table>
