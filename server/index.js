f53f2829bbf33d126a287e795f8760fae26b7c51
Jonas Fender <Jonas.Fender@smail.th-koeln.de>
2019-07-14 21:11:55 +0200

    Userfunktionen fertig implementiert

--- server/index.js
+++ server/index.js
@@ -15,14 +15,14 @@ app.get('/getgames', function (req, res) {
       {
         from: 'users',
         localField: 'p1id',
-        foreignField: '_id',
+        foreignField: 'username',
         as: 'p1details'
       }},
       { $lookup:
         {
         from: 'users',
         localField: 'p2id',
-        foreignField: '_id',
+        foreignField: 'username',
         as: "p2details"
       }
     }
@@ -34,32 +34,73 @@ app.get('/getgames', function (req, res) {
   });
 
 })
-
+/*
+Post needs json in this format
+The list t1/t2 is flexible. You can add 1 or infinite players
+{
+  t1:[
+  { pid: "{username from player 1 in team 1}"},
+  {  pid: "{username from player 2 in team 2}"}],
+  t2:[
+  { pid: "{username from player 1 in team 2}"},
+  {  pid: "{username from player 2 in team 2}"}],
+  t1score: {score from team1},
+  t2score: {score from team2}
+}
+*/
 app.post('/addgames', function (req, res) {
+  var op1 = false;
+  var op2 = false;
+
+var json=[];
+  function buildJson(team) {
+    console.log(team[0])
+    console.log(Object.keys(team).length)
+    for(var i = 0;i<Object.keys(team).length;i++){
+      json+='{"pid":'+(team[i].pid)+'}'
+      if(i<(Object.keys(team).length)-1) {
+        json+=','
+      }
+      console.log(json)
+    }
+    return JSON.parse(json.toString)
+  }
+
 
 
   MongoClient.connect(url, {useNewUrlParser:true}, function(err, db) {
     if(err) throw err;
     var dbo = db.db("elo");
     dbo.collection("games").insertOne({
-      p1id: new ObjectId(req.body.p1id),
-      p2id: new ObjectId(req.body.p2id),
-      p1score: req.body.p1score,
-      p2score: req.body.p2score
+    t1:[
+      buildJson(req.body.t1)
+    ],
+    t2:[
+    buildJson(req.body.t2)
+    ],
+    t1score: req.body.t1score,
+    t2score: req.body.t2score,
+    timestamp: Date.now()
+
     },function(err, result) {
       if (err) throw err;
       db.close();
-      res.sendStatus(200);
+      res.sendStatus(200)
     })
   })
+/*  MongoClient.connect(url, {useNewUrlParser}, function(req, res) {
+    if(err) throw err;
+    var dbo = db.db("elo");
+    dbo.collection("users").update
+  })*/
+
 })
 
 
 /*
 Post needs json in this format
 {
-"username":"{somename}"",
-"elo":{someInt}
+  username:"{somename}"
 }
 */
 app.post('/adduser', function ( req, res) {
@@ -68,13 +109,17 @@ app.post('/adduser', function ( req, res) {
   MongoClient.connect(url, {useNewUrlParser:true}, function(err, db) {
     if(err) throw err;
     var dbo = db.db("elo");
-    dbo.collection("users").insertOne(req.body, function(err, result) {
+    dbo.collection("users").insertOne({
+      username: req.body.username,
+      elo: 1000
+    }, function(err, result) {
       if (err) throw err;
       db.close();
       res.sendStatus(200);
     })
   })
 })
+
 //If ID = 0, you get the complete List
 app.get('/getusers/:id', function (req, res) {
   MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
@@ -118,9 +163,9 @@ app.get('/testuser', function (req, res) {
       console.log("Number of documents inserted: " + res.insertedCount);
       console.log("break");
       console.log(res);
-      db.close();
-    });
-  });
+    })
+    db.close()
+  })
 
 
 })
@@ -130,15 +175,59 @@ app.get('/testgames', function (req, res) {
     if (err) throw err;
     var dbo = db.db("elo");
     var myobj = [
-      { p1id: new ObjectId("5d29cf2507f8ab2b9787bf13"), p2id: new ObjectId("5d29cf2507f8ab2b9787bf14"), p1score: 10, p2score: 6},
-      { p1id: new ObjectId("5d29cf2507f8ab2b9787bf17"), p2id: new ObjectId("5d29cf2507f8ab2b9787bf15"), p1score: 2, p2score: 10}
+      {
+        t1: [
+            {pid: "John"},
+            {pid: "Sandy"},
+        ],
+        t2: [
+            {pid: "Betty"},
+            {pid: "Michael"}
+        ],
+        t1score: 3,
+        t2score: 10,
+        timestamp: Date.now()
+    },
+    {
+      t1: [{
+          pid: "Sandy"
+      }],
+      t2: [{
+          pid: "Hannah"
+      }],
+      t1score: 10,
+      t2score: 5,
+      timestamp: Date.now()
+  }
     ];
     dbo.collection("games").insertMany(myobj, function(err, res) {
       if (err) throw err;
       console.log("Number of documents inserted: " + res.insertedCount);
       db.close();
-    });
-  });
+    })
+  })
+})
+
+app.get('/test', function (req, res) {
+  var json = {
+    t1: [{
+        pid: "5d29cf2507f8ab2b9787bf18"},
+        {pid: "5d29f86c03058f3cad56fc7f"},
+    ],
+    t2: [{
+        pid: "5d29f86c03058f3cad56fc7f"},
+        {pid: "5d29f86c03058f3cad56fc7f"
+    }],
+    p1score: 8,
+    p2score: 2,
+    timestamp: Date.now()
+}
+
+console.log(Object.keys(json.t1).length);
+//console.log(Date.now())
+console.log(json.t1[0])
+console.log(json);
+res.send(json.t1)
 })
 
 
