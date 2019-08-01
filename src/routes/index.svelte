9f5e284f06d83e9d870ae40d71f8dd95eae8ee91
Simon Marcel Linden <simon_marcel.linden@smail.th-koeln.de>
2019-08-01 22:11:19 +0200

    Added Dashboard
    Dashboard added. Content of the template Index.svelte replaced by a dashboard

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,63 +24,76 @@
 	table {
 		font-size: var(--s0);
 	}
-
 	th {
 		padding: var(--s1) var(--s3);
 	}
-
 	td {
 		padding-bottom: var(--s-5);
 	}
-
 	td, th {
 		text-align: center;
 		vertical-align: middle;
 	}
 
 
+    .content-header{
+        position: relative;
+        padding : 15px 15px 0 15px;
+    }
+        .content-header > h1,
+            .content-header > h1 > small {
+            display  	: inline-block;
+            font-size 	: 15px;
+            padding-left: 4px;
+            font-weight : 300;
+        }
+    .content{
+        min-height 		: 250px;
+        padding 		: 15px;
+        margin-right 	: auto;
+        margin-left 	: auto;
+    }
+    .flex-layout{
+        display 		    	: flex;
+        flex-direction 			: column;
+        flex-wrap 				: nowrap;
+        grid-gap 				: 10px;
+        grid-template-columns 	: repeat(12, [col] 1fr);
+        grid-template-rows 		: repeat(12, [row] 1fr);
+        color 					: var(--black);
+        margin 					: 0 auto;
+    }
+        .flex-row{
+            padding-bottom 	: 7px;
+            margin-bottom 	: 7px;
+            /*border 			: 1px solid var(--grey);*/
+        }
+            div[class*="flex-field-"]{
+                padding-right: 14px;
+                float 		: left;
+            }
+            div[class*="flex-field-"]:last-child{
+                padding-right: 0;
+            }
+                .flex-field-12{
+                    width: 100%;
+                }
+                .flex-field-8{
+                    width: 66.66666667%;
+                }
+                .flex-field-4{
+        width: 33.33333333%;
+    }
 </style>
 
-<table>
-	<tr class="red">
-		<th># rank</th>
-		<th># username</th>
-		<th># elo</th>
-		<th># wins</th>
-		<th># games</th>
-		<th># TD</th>
-	</tr>
-	{#each users as user, rank}
-	<tr>
-		<td>{rank + 1}</td>
-		<td>{user.username}</td>
-		<td>{user.elo}</td>
-		<td>{user.wins}</td>
-		<td>{user.games}</td>
-		<td class="score_difference"
-				class:red={calcDifference(user.goals ,user.goals_conceded) < 0 }
-				class:green={calcDifference(user.goals ,user.goals_conceded) > 0 }
-				class:blue={calcDifference(user.goals ,user.goals_conceded) == 0 }
-		>{calcDifference(user.goals ,user.goals_conceded)}</td>
-	</tr>
-	{/each}
-</table>
-
-<table>
-	<tr class="red">
-		<th># Heim</th>
-		<th># Tore</th>
-		<th># Gast</th>
-		<th># Tore</th>
-		<th># Spieltag</th>
-	</tr>
-	{#each games as game}
-	<tr>
-		<td>{#each game.usersOne as player } {player}<br> {/each}</td>
-		<td>{game.scoreOne}</td>
-		<td>{#each game.usersTwo as player } {player}<br> {/each}</td>
-		<td>{game.scoreTwo}</td>
-		<td>{game.timestamp}</td>
-	</tr>
-	{/each}
-</table>
+<div id="dashboard">
+    <section class="content-header">
+        <h1 class="no-margin">Dashboard<small>Version 1.0</small></h1>
+    </section>
+    <section class="content flex-layout content no-center">
+        <div class="flex-row"></div>
+        <div class="flex-row"></div>
+        <div class="flex-row"></div>
+        <div class="flex-row"></div>
+    </section>
+</div>
