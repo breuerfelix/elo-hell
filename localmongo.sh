73e35d71a651547c31f53f3d92c2a14d9d0a4d48
Felix Breuer <f.breuer94@gmail.com>
2020-02-16 18:28:52 +0100

    fix telegram webhook
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>

--- localmongo.sh
+++ localmongo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run -p 27017:27017 --name elo-mongo -v ${PWD}/elo-db:/data/db --rm mongo:latest
+podman run -p 27017:27017 --name elo-mongo --rm mongo:latest
--- src/routes/telegram.json.js
+++ src/routes/telegram.json.js
@@ -1,11 +1,13 @@
 export async function post(req, res) {
 	const { telegramBot, body } = req;
 
+	res.writeHead(200);
+
 	if (!telegramBot) {
-		res.sendStatus(500);
+		res.end();
 		return;
 	}
 
 	telegramBot.processUpdate(body);
-	res.sendStatus(200);
+	res.end();
 }
